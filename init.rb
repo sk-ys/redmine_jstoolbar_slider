51e73e30e7db17bf673db536030a387c14cbb9d3
sk-ys <yskrmt@gmail.com>
2023-08-06 22:00:26 +0900

    v0.1.1

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_jstoolbar_slider do
   name "Redmine jsToolbar Slider plugin"
   author "sk-ys"
   description "This is a plugin for Redmine"
-  version "0.1.0"
+  version "0.1.1"
   url "https://github.com/sk-ys/redmine_jstoolbar_slider"
   author_url "https://github.com/sk-ys"
 end
